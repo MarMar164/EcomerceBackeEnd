b6382baa62ca3a9eb4df72bad18b9a40991c39b6
Marquell <starjet164@gmail.com>
2022-10-30 19:01:09 -0500

    created first get rout

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,25 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(dbCData => {
+      if (!dbCData) {
+        res.status(404).json({
+          message: 'No Categories found'
+        });
+        return;
+      }
+      res.json(dbCData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    })
 });
 
 router.get('/:id', (req, res) => {
