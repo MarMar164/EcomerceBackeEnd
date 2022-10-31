1d6a0adb3cd0cb08e49d7c9628aa7cbb67fa707a
Marquell <starjet164@gmail.com>
2022-10-30 20:06:17 -0500

    find all project rouut

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -52,20 +52,58 @@ router.get('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err)
     })
-
-
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(dbCData => res.json(dbCData))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-});
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCData => {
+      if (!dbCData) {
+        res.status(404).json({ message: 'No category found with this id' })
+        return;
+      }
+      res.json(dbCData)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+})
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.delete({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCData => {
+      if (!dbCData) {
+        res.status(404).json({ message: 'No category found with that id' })
+        return;
+      }
+      res.json(dbCData);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
