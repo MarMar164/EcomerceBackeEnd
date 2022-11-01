c654880da924ab096eb204d339a62ba5ef32d201
Marquell <starjet164@gmail.com>
2022-10-31 22:15:12 -0500

    crated root for new product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -64,14 +64,31 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+ Product.create({
+price: req.body.price,
+stock: req.body.stock,
+category_id: req.body.category_id,
+tagIds: req.body.tagIds
+ })
+ .then((product) => {
+if (req.body.tagIds.length){
+const productTagIdArr = req.body.tagIds.map((tag_id) => {
+return{
+product_id: product.id,
+tag_id,
+};
+});
+return ProductTag.bulkCreate(productTagIdArr);
+}
+res.status(200).json(product);
+ })
+.then((productTagIds) => res.status(200).json(productTagIds))
+.catch((err) => {
+console.log(err);
+res.status(400).json(err)
+})
+
+ })
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -92,7 +109,7 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
+
 
 // update product
 router.put('/:id', (req, res) => {
